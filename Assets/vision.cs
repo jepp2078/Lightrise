67c03b080c933bd14e479772fc8719e5a640cd64
jepp2078 <jepp2078@gmail.com>
2015-05-01 15:02:49 +0200

    Update vision.cs

--- Assets/vision.cs
+++ Assets/vision.cs
@@ -5,13 +5,14 @@ public class vision : MonoBehaviour
 {
 
 /*
- *So. Your character skills and items are persistent.
+ * Your character skills and items are persistent.
  * The servers are also persistent and can hold something like 20 player(Still undecided).
  * The idea is that each server has zones you can capture/siege, like in darkfall, but the twist is that you can build on your own land ala minecraft.
  * Since the skill system is pretty extensive, each server would have a prebuild, "no building allowed", town in the middle where the basic skills can be bought.
  * The more advanced skills would be purchasable in "clan" build cities, meaning that the bigger you build your city, the more advanced npc's will move in that you can buy skills from.
  * (Think something like build a forge and a blacksmith will move in besides it).
- * The world will be filled with resources, and rare resources, you can gather and build/craft from.
- * Enemy npc's are still undecided. Not sure what to do about those, or if i even want them. */
+ * The world will be filled with resources, and rare resources, you can gather and build/craft from. 
+   Everything in your inventory will drop on death and can be looted by other players. 
+   There will be some kind of skill cap to limit your final build.*/
 	
 }
